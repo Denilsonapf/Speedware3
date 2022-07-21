7ded836718d0d3126e56cf3b30d9b4478bd25995
Denilsonapf <denilson.ferreira@uerj.br>
2022-07-21 02:53:59 -0300

    Add React
    Co-authored-by: Elson Santos <elsonss1988@users.noreply.github.com>

--- react/WishlistButton.js
+++ react/WishlistButton.js
@@ -1,16 +1,13 @@
-import React from "react"
-import Teste from './components/wishlist/index.js'
+import React from "react";
+import Teste from "./components/wishlist/index.js";
 
-
-const teste = new Teste()
+const teste = new Teste();
 
 const WishlistButton = () => {
-  teste.oi()
-  teste.req()
-  teste.order()
-  return(
-    <div> --- </div>
-  )
-}
+  teste.oi();
+  teste.req();
+  teste.order();
+  return <div> --- </div>;
+};
 
-export default WishlistButton
+export default WishlistButton;
